b8952f6a980a1f30d0c00b846a3174560c4666a7
karlfleener <karlfleener@gmail.com>
2020-10-27 09:23:46 -0400

    update portfolio

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 $('a').click(function() {
     let sectionTo = $(this).attr('href');
+    let scrollAmount = sectionTo === '#hero' ? 0 : $(sectionTo).offset().top - 74
+
     $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top
+      scrollTop: scrollAmount,
     }, 1000);
+});
+
+
+$(window).scroll(function() {    
+    let scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#main-header").addClass("nav-shadow");
+    } else {
+        $("#main-header").removeClass("nav-shadow");
+    }
 });
